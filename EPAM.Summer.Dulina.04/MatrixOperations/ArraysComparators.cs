73a0d9b31d349f93fdae63910f24dee0b36c6c64
Артем <@->
2016-07-07 21:01:37 +0300

    add first task with comparators(not effective) and also without comparators(several methods)

---
+++ EPAM.Summer.Dulina.04/MatrixOperations/ArraysComparators.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixOperations
+{
+    public class SortBySumOfTheElements : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            int[] a = x as int[];
+            int[] b = y as int[];
+            int sumA = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sumA += a[i];
+            }
+            int sumB = 0;
+            for (int i = 0; i < b.Length; i++)
+            {
+                sumB += b[i];
+            }
+            if (sumA > sumB)
+                return 1;
+            else if (sumA < sumB)
+                return -1;
+            else return 0;
+        }
+    }
+
+    public class SortByMaximumOfTheElements : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            int[] a = x as int[];
+            int[] b = y as int[];
+            int maxA = int.MinValue;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (maxA < a[i])
+                    maxA = a[i];
+            }
+            int maxB = int.MinValue;
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (maxB < b[i])
+                    maxB = b[i];
+            }
+            if (maxA > maxB)
+                return 1;
+            else if (maxA < maxB)
+                return -1;
+            else return 0;
+        }
+    }
+
+    public class SortByMinimumOfTheElements : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            int[] a = x as int[];
+            int[] b = y as int[];
+            int minA = int.MaxValue;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (minA > a[i])
+                    minA = a[i];
+            }
+            int minB = int.MaxValue;
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (minB > b[i])
+                    minB = b[i];
+            }
+            if (minA > minB)
+                return 1;
+            else if (minA < minB)
+                return -1;
+            else return 0;
+        }
+    }
+}
---
+++ EPAM.Summer.Dulina.04/MatrixOperations/GroupOperations.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixOperations
+{
+
+    public static class GroupOperations
+    {
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        private static void Swap(ref int[] a, ref int[] b)
+        {
+            int[] temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static int[] FindMaxOfEachRow(int[][] array)
+        {
+            int max = int.MinValue;
+            int[] maxValues = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (array[i][j] > max)
+                        max = array[i][j];
+                }
+                maxValues[i] = max;
+                max = int.MinValue;
+            }
+            return maxValues;
+        }
+
+        public static int[] FindMinOfEachRow(int[][] array)
+        {
+            int min = int.MaxValue;
+            int[] minValues = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (array[i][j] < min)
+                        min = array[i][j];
+                }
+                minValues[i] = min;
+                min = int.MaxValue;
+            }
+            return minValues;
+        }
+
+        public static int[] FindSumOfEachRow(int[][] array)
+        {
+            int[] sumValues = new int[array.Length];
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    sum += array[i][j];
+                }
+                sumValues[i] = sum;
+                sum = 0;
+            }
+            return sumValues;
+        }
+
+        public static IComparer SortRowsBySumOfTheElements
+        {
+            get { return new SortBySumOfTheElements(); }
+        }
+
+        public static IComparer SortRowsByMaximumOfTheElements
+        {
+            get { return new SortByMaximumOfTheElements(); }
+        }
+
+        public static IComparer SortRowsByMinimumOfTheElements
+        {
+            get { return new SortByMinimumOfTheElements(); }
+        }
+
+        public static void SortingByRows(int[][] array, IComparer comparer, bool reverse = false)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                for (var j = 1; j < array.Length - i; j++)
+                {
+                    if (comparer.Compare(array[j], array[j - 1]) == -1)
+                        Swap(ref array[j], ref array[j - 1]);
+                }
+            }
+
+            if (reverse)
+            {
+                for (int i = 0; i < array.Length / 2; i++)
+                {
+                    Swap(ref array[i], ref array[array.Length - 1 - i]);
+                }
+            }
+        }
+
+        public static void SortArrayByMaximumOfTheElements(int[][] array, bool reverse = false)
+        {
+            int[] helper = FindMaxOfEachRow(array);
+
+            SimpleSort(array, helper, reverse);
+        }
+
+        public static void SortArrayByMinimumOfTheElements(int[][] array, bool reverse = false)
+        {
+            int[] helper = FindMinOfEachRow(array);
+
+            SimpleSort(array, helper, reverse);
+        }
+
+        public static void SortArrayBySumOfTheElements(int[][] array, bool reverse = false)
+        {
+            int[] helper = FindSumOfEachRow(array);
+
+            SimpleSort(array, helper, reverse);
+        }
+
+        private static void SimpleSort(int[][] array, int[] helper, bool reverse = false)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                for (var j = 1; j < array.Length - i; j++)
+                {
+                    if (helper[j] < helper[j - 1])
+                    {
+                        Swap(ref array[j], ref array[j - 1]);
+                        Swap(ref helper[j], ref helper[j - 1]);
+                    }
+                }
+            }
+
+            if (reverse)
+            {
+                for (int i = 0; i < array.Length / 2; i++)
+                {
+                    Swap(ref array[i], ref array[array.Length - 1 - i]);
+                }
+            }
+        }
+    }
+}
